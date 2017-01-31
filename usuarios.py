47db876123774a0ca49a34e5486a8e87e77c4e4a
Seila Oliva Munoz <soliva@gamma14.aulas.gsyc.urjc.es>
2017-01-31 12:47:28 +0100

    Modificado añadiendo los elementos a un diccionario. Falta resolver excepcion

--- usuarios.py
+++ usuarios.py
@@ -4,10 +4,14 @@ fich = open("/etc/passwd", "r")
 users = fich.readlines()
 fich.close()
 
+shells = {}
+
 for user in users:
 	login = user.split(":")[0]
-	shell = user.split(":")[-1]
-	print("usuario: " + login + ", shell: " + shell, end="")
-	#En vez de poner end="", se puede quitar el \n de la variable shell: user.split(":")[-1][:-1]
+	shell = user.split(":")[-1][:-1]
+	shells[login] = shell
+
+print(shells['root'])
+print(shells['imaginario'])
 
 print("Número de usuarios: " + str(len(users)))
